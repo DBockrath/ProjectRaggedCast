503eb76ec27a6b52992653d997a77869acf93a5b
DTMSoundMaster <dtmsoundcorps@gmail.com>
2017-06-14 07:16:11 -0400

    debugging3

--- ProjectRaggedCast/app/src/main/java/com/DTMSoundMaster/ProjectRaggedCast/MainActivity.java
+++ ProjectRaggedCast/app/src/main/java/com/DTMSoundMaster/ProjectRaggedCast/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 		
-		String fontPath = "@drawable/good_dog.otf";
+		String fontPath = "drawable/good_dog.otf";
 		TextView txt1 = (TextView)findViewById(R.id.title);
 		Typeface tf = Typeface.createFromAsset(getAssets(), fontPath);
 		txt1.setTypeface(tf);
